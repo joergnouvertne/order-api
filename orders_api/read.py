8c67fefe1adaaed0a382f86c8af83e1f9fbfa3d3
joerg <joerg.nouvertne@posteo.de>
2022-04-03 12:27:38 +0200

    Initial commit

---
+++ orders_api/read.py
@@ -0,0 +1,18 @@
+import simplejson as json
+import boto3
+import os
+from boto3.dynamodb.conditions import Key
+
+
+def lambda_handler(event, context):
+    dynamodb = boto3.resource('dynamodb')
+    table_name = os.environ.get('ORDERS_TABLE')
+    table = dynamodb.Table(table_name)
+    order_id = int(event['pathParameters']['id'])
+    response = table.query(KeyConditionExpression=Key('id').eq(order_id))
+
+    return {
+        'statusCode': 200,
+        'headers': {},
+        'body': json.dumps(response['Items'])
+    }
